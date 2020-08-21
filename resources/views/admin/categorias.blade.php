7d2e47e786eeda0d28b0e7ba8dd128d0742db96a
Jorge Henrique <jorgehenrique@live.com>
2020-08-20 21:36:09 -0300

    Edit Layout and New Modal Post

--- resources/views/admin/categorias.blade.php
+++ resources/views/admin/categorias.blade.php
@@ -1,9 +1,11 @@
 @extends('layout.admin')
 
+@section('title', 'Categorias')
+
 @section('content')
 
 <div class="container p-4">
-<h2>Lista de Categorias</h2>
+  <h2 class="title_dash">Lista de Categorias   <button data-toggle="modal" data-target="#exampleModal" class="btn btn-xs btn-new btn-primary float-right d-flex align-items-center">Cadastrar Novo <span class="material-icons">add</span></button></h2>
     <table class="table">
       <thead>
         <tr>
@@ -21,11 +23,38 @@
               <td>{{ $loop->iteration }}</td>
               <td>{{ $c['name'] }}</td>
               <td>{{ $c['keywords'] }}</td>
-              <td>Editar</td>
-              <td>Excluir</td>
+              <td width="5%"><button class="btn btn-xs btn-size btn-info d-flex align-items-center"><span class="material-icons">create</span></button></td>
+              <td width="5%"><button class="btn btn-xs btn-size btn-danger d-flex align-items-center"><span class="material-icons">delete</span></button></td>
             </tr>
         @endforeach
       </tbody>
     </table>
+</div>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Cadastrar Categoria</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="nameCateg">Nome:</label>
+            <input type="text" name="category" class="form-control" id="nameCateg" placeholder="Nome da Categoria">
+          </div>
+          <div class="form-group">
+            <label for="keywordsCateg">KeyWords:</label>
+            <input type="text" name="keywords" class="form-control" id="keywordsCateg" placeholder="Palavras chaves separadas por virgula">
+          </div>
+          <div class="row d-flex justify-content-center mt-4">
+            <button type="submit" class="btn btn-submit btn-success">Cadastrar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Fênix Admin @hasSection('title') - @yield('title') @endif</title>
 
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
@@ -32,11 +32,13 @@
     </head>
     <body>
         <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 d-flex justify-content-center" href="#">
+                <img src="{{ asset('assets/images/fenix-hookah.svg') }}" width="70px" class="img-fluid" alt="Logo fenix" />
+            </a>
             <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+           
             <ul class="navbar-nav px-3">
                 <li class="nav-item text-nowrap">
                     <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
@@ -52,44 +54,28 @@
         </nav>
         <div class="container-fluid">
         <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse" >
                 <div class="sidebar-sticky pt-3">
                     <ul class="nav flex-column">
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">
+                        <a class="nav-link {{ (request()->routeIs('home')) ? 'active' : '' }}" href="{{route('home')}}">
                             <span class="material-icons">home</span>
                             Home
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">
+                        <a class="nav-link {{ (request()->routeIs('category')) ? 'active' : '' }}" href="{{route('category')}}">
                         <span class="material-icons">category</span>
                          Categorias
                         </a>
                     </li>
                     </ul>
-
-                    <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                    <span>Saved reports</span>
-                    <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-                        <span data-feather="plus-circle"></span>
-                    </a>
-                    </h6>
-                    <ul class="nav flex-column mb-2">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        <span data-feather="file-text"></span>
-                        Current month
+                        {{--  <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                        <span>Saved reports</span>
+                        <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+                            <span data-feather="plus-circle"></span>
                         </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        <span data-feather="file-text"></span>
-                        Last quarter
-                        </a>
-                    </li>
-                    
-                    </ul>
+                        </h6>  --}}
                 </div>
             </nav>
             <main id="app" role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
